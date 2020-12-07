2033e9470c2c9f6133622a3ed2ff24612c4cd053
MartinL3dl <60381918+MartinL3dl@users.noreply.github.com>
2020-12-07 15:54:51 +0100

    Create salaryfunction.php

---
+++ salaryfunction.php
@@ -0,0 +1,26 @@
+<?php
+function cleanSalary(int $hardSalary, bool $isStudent = FALSE):int { // začátek těla funkce
+    if($isStudent == TRUE) {
+        $cleanSalary = $hardSalary;
+    } else {
+        $cleanSalary = $hardSalary * 0.90;
+    }
+    return $cleanSalary;
+} 
+ 
+function cleanSalaryComplex(int $hardSalary, string $type = "") {
+     
+    switch ($type) {
+        case 'student':
+            $tax = 0;
+            break;
+        case 'korona':
+            $tax = 0.05;
+            break;
+        default:
+            $tax = 0.10;
+            break;
+    }
+    $cleanSalary = $hardSalary * (1 - $tax);
+    return $cleanSalary;
+}
